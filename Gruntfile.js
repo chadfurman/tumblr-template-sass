da9958b96a746c7fb1ac6a3dc2819113ee3289c1
Chad Furman <chad@fuzzproductions.com>
2014-05-10 22:26:29 -0400

    changed gruntfile to use preprocess for include directives instead of just concatinating everything together

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,14 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		concat: {
-			dist: {
-				src: [
-					'_header.tumblr',
-					'_blockAnswer.tumblr',
-					'_blockAudio.tumblr',
-					'_blockChat.tumblr',
-					'_blockLink.tumblr',
-					'_blockPhoto.tumblr',
-					'_blockPhotoset.tumblr',
-					'_blockPosts.tumblr',
-					'_blockQuote.tumblr',
-					'_blockText.tumblr',
-					'_blockVideo.tumblr',
-					'_footer.tumblr'
-				],
-				dest: "template.tumblr"
+		preprocess : {
+			html : {
+				src : 'templates/main.tumblr',
+				dest : 'theme.tumblr'
 			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.registerTask('default', ['concat']);
+
+	grunt.loadNpmTasks('grunt-preprocess');
+	grunt.registerTask('default', ['preprocess']);
 }

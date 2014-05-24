1bdd2d60f35784ef03e8043c5b026f2e95c5458a
Chad Furman <chad@fuzzproductions.com>
2014-05-24 15:59:01 -0400

    concatting all the tumblr files into one before running preprocess

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,34 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			dist: {
+				src: [
+					'templates/_header.tumblr',
+//					'_blockAnswer.tumblr',
+//					'_blockAudio.tumblr',
+//					'_blockChat.tumblr',
+//					'_blockLink.tumblr',
+//					'_blockPhoto.tumblr',
+//					'_blockPhotoset.tumblr',
+//					'_blockPosts.tumblr',
+//					'_blockQuote.tumblr',
+//					'_blockText.tumblr',
+//					'_blockVideo.tumblr',
+					'templates/_footer.tumblr'
+				],
+				dest: "build/unprocessed.template.tumblr"
+			}
+		},
 		preprocess : {
 			html : {
-				src : 'templates/main.tumblr',
+				src : 'build/unprocessed.template.tumblr',
 				dest : 'theme.tumblr'
 			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-preprocess');
-	grunt.registerTask('default', ['preprocess']);
+	grunt.registerTask('default', ['concat', 'preprocess']);
 }

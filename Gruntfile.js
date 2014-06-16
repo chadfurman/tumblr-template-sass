ed5cff72d384befd3d94fe0dcd1851efa48c3dee
Chad Furman <chad@fuzzproductions.com>
2014-06-15 20:58:39 -0400

    updated libraries with new bower folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,10 +22,10 @@ module.exports = function(grunt) {
 		concat: {
 			js: {
 				src: [
-					'bower_components/jquery/dist/jquery.js',
-					'bower_components/foundation/js/foundation/foundation.js',
-					'bower_components/foundation/js/foundation/foundation.magellan.js',
-					'theme/js/pxuPhotoset.js',
+					'libs/bower_components/jquery/dist/jquery.js',
+					'libs/bower_components/foundation/js/foundation/foundation.js',
+					'libs/bower_components/foundation/js/foundation/foundation.magellan.js',
+					'libs/tumblr-photoset/tumblr-photoset.js',
 					'theme/js/theme.js'
 				],
 				dest: 'theme.js',

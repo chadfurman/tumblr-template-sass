624c248da01dec54dced9674ed01adb5a0b4ffaa
Chad 'ShadesOfLight' Furman <chad@chadfurman.com>
2014-06-19 00:20:49 -0400

    put require.config in seperate file.  Running photoset plugin once per photoset

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,27 +1,21 @@
-require.config({
-	baseUrl: "/libs",
-	paths: {
-		"jquery": "bower_components/jquery/jquery.js"
-	},
-	waitSeconds: 15
-});
-require( ["jquery"],
-	function($) {
-		console.log('thank god!  it worked.');
+require( ["jquery", "foundation", "foundation.magellan", "tumblrPhotoset"],
+	function($, foundation) {
 		$(document).foundation();
 
 		$(document).ready(function() {
-			console.log($('.photo-slideshow').tumblrPhotoset({
-				'ligthbox'  : true, // uses the default Tumblr lightbox, change to false to use your own
-				'highRes'   : true, // will use high res images
-				'rounded'   : 'corners', // corners, all or false
-				'exif'      : true, // display EXIF data if available (tooltip)
-				'captions'  : true, // display individual captions on photos (tooltip)
-				'gutter'    : '10px', // spacing between the images
-				'photoset'  : '.photo-slideshow', // photoset wrapper
-				'photoWrap' : '.photo-data', // photo data wrapper (includes photo, icons + exif)
-				'photo'     : '.pxu-photo' // photo wrap (includes photo only)
-			}));
+			$('.photo-slideshow').each(function() {
+				$(this).tumblrPhotoset({
+					'ligthbox'  : true, // uses the default Tumblr lightbox, change to false to use your own
+					'highRes'   : true, // will use high res images
+					'rounded'   : 'corners', // corners, all or false
+					'exif'      : true, // display EXIF data if available (tooltip)
+					'captions'  : true, // display individual captions on photos (tooltip)
+					'gutter'    : '10px', // spacing between the images
+					'photoset'  : '.photo-slideshow', // photoset wrapper
+					'photoWrap' : '.photo-data', // photo data wrapper (includes photo, icons + exif)
+					'photo'     : '.pxu-photo' // photo wrap (includes photo only)
+				});
+			});
 		});
 	}
 );

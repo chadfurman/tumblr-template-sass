77aa2229abfc55a05bf0fd6e9184a2209a8d0dbb
Chad 'ShadesOfLight' Furman <chad@chadfurman.com>
2014-06-19 00:20:07 -0400

    added requirejs to grunttask

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,50 +20,52 @@ module.exports = function(grunt) {
 			}
 		},
 		concat: {
-			js: {
-				src: [
-					'libs/bower_components/foundation/js/foundation/foundation.js',
-					'libs/bower_components/foundation/js/foundation/foundation.magellan.js',
-					'libs/tumblr-photoset/tumblr-photoset.js',
-					'theme/js/main.js'
-				],
-				dest: 'theme.js',
-			},
 			css: {
 				src: ['theme/css/screen.css', 'theme/css/pxuPhotoset.css'],
-				dest: 'theme.css',
+				dest: 'theme.css'
+			}
+		},
+		requirejs: {
+			compile: {
+				options: {
+					baseUrl: "./",
+					mainConfigFile: "theme/js/require.config",
+					name: "theme/js/main.js",
+					out: "theme.js"
+				}
 			}
 		},
 		watch: {
 		  scripts: {
-			files: ['theme/js/*.js'],
-			tasks: ['concat:js'],
+			files: ['theme/js/*.js', 'libs/tumblr-photoset/*.js'],
+			tasks: ['requirejs'],
 			options: {
-			  spawn: false,
-			},
+			  spawn: false
+			}
 		  },
 		  styles: {
 			files: ['theme/sass/*', 'theme/sass/post_types/*'],
 			tasks: ['compass', 'concat:css'],
 			options: {
-			  spawn: false,
-			},
+			  spawn: false
+			}
 		  },
 		  templates: {
 			files: ['theme/templates/*', 'theme/templates/partials/*'],
 			tasks: ['preprocess'],
 			options: {
-			  spawn: false,
-			},
-		  },
-		},
+			  spawn: false
+			}
+		  }
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-preprocess');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask(
-		'default', ['preprocess', 'compass', 'concat', 'watch']
+		'default', ['preprocess', 'compass', 'requirejs', 'concat', 'watch']
 	);
 }

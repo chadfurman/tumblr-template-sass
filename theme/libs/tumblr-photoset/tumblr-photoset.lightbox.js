096e23522d7d597f7cbfb60f331de83543a1b269
Chad Furman <chad@fuzzproductions.com>
2014-06-22 09:53:33 -0400

    began refactoring lightbox

--- theme/libs/tumblr-photoset/tumblr-photoset.lightbox.js
+++ theme/libs/tumblr-photoset/tumblr-photoset.lightbox.js
@@ -17,39 +17,29 @@ $('.tumblr-box').on('click', function (e) {
 
 });
 
-// add a count number to each image so that we know where
-// to start the lightbox when we init
+// add a count number to each image so that we know where to start the lightbox when we init
 for(i = 0; i < allImages.length; i++) {
   // @todo figure out how to pass allImages into the extension...
   var image = allImages.eq(i);
   image.attr('data-count', i + 1);
 }
 
-var tumblrLightbox = function (current,photoset) {
+// figure out which image was clicked
+// we'll make sure that's where we start our lightbox
+var openWith = current.parents(settings.photoWrap).find(settings.photo+' img').data('count');
 
-  // figure out which image was clicked
-  // we'll make sure that's where we start our lightbox
-  var openWith = current.parents(settings.photoWrap).find(settings.photo+' img').data('count');
+var imageWidth = thisImage.data('width');
+var imageHeight = thisImage.data('height');
+var imageLowRes = thisImage.attr('src');
+var imageHighRes = thisImage.data('highres');
 
-  // setup array of images
-  var photosetArray = [];
-  $('#'+photoset).find(settings.photoWrap).each(function () {
-    var thisImage = $(this).find(settings.photo+' img');
-    var imageWidth = thisImage.data('width');
-    var imageHeight = thisImage.data('height');
-    var imageLowRes = thisImage.attr('src');
-    var imageHighRes = thisImage.data('highres');
+// formatting is specific to how Tumblr has things set up
+var thisPhotoPackage = {
+  width    : imageWidth,
+  height   : imageHeight,
+  low_res  : imageLowRes,
+  high_res : imageHighRes
+};
+photosetArray.push(thisPhotoPackage);
 
-    // formatting is specific to how Tumblr has things set up
-    var thisPhotoPackage = {
-      width    : imageWidth,
-      height   : imageHeight,
-      low_res  : imageLowRes,
-      high_res : imageHighRes
-    };
-    photosetArray.push(thisPhotoPackage);
-  });
-
-  Tumblr.Lightbox.init(photosetArray, openWith);
-
-}; // end tumblrLightbox()
+Tumblr.Lightbox.init(photosetArray, openWith);

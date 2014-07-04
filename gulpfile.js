0a9b2ba301fff5a777f42bd8e2d4c2d1b89f6c26
Chad Furman <chad@fuzzproductions.com>
2014-07-04 11:43:11 -0400

    added clipboard to gulp file on HTML compile

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var browserify = require('browserify');
 var preprocess = require('gulp-preprocess');
 var compass    = require('gulp-compass');
 var watch      = require('gulp-watch');
-var plumber    = require('gulp-plumber');
+var clipboard  = require('gulp-clipboard');
 var source     = require('vinyl-source-stream');
 
 gulp.task('scripts', function() {
     return browserify('./theme/js/main.js')
         .bundle()
-        .on('error', console.log)
+        .on('error', watchTask)
         //Pass desired output filename to vinyl-source-stream
         .pipe(source('./build/theme.js'))
         // Start piping stream to tasks!
@@ -19,8 +19,9 @@ gulp.task('scripts', function() {
 gulp.task('html', function() {
   gulp.src('theme/templates/main.tumblr')
     .pipe(preprocess()) //To set environment variables in-line
-    .on('error', console.log)
-    .pipe(gulp.dest('build/theme.tumblr'))
+    .on('error', watchTask)
+    .pipe(clipboard())
+    .pipe(gulp.dest('build/theme.tumblr'));
 });
 
 gulp.task('styles', function() {
@@ -32,14 +33,16 @@ gulp.task('styles', function() {
       sass: 'theme/sass',
       import_path: 'theme/libs/bower_components/foundation/scss'
     }))
-    .on('error', console.log)
+    .on('error', watchTask)
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', watchTask);
+
+gulp.task('default', ['scripts', 'html', 'styles', 'watch']);
+
+function watchTask() {
   gulp.watch('theme/sass/**/*.scss', ['styles']);
   gulp.watch('theme/templates/**/*.tumblr', ['html']);
   gulp.watch(['theme/js/**/*.js', 'theme/libs/**/*.js'], ['scripts']);
-});
-
-gulp.task('default', ['scripts', 'html', 'styles', 'watch']);
+}

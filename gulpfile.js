2d841bc9b275d28c3a1550d28fd08c34fc49457f
Chad 'ShadesOfLight' Furman <chad@chadfurman.com>
2015-02-14 18:42:22 -0500

    rebuilt gulpfile to produce a distributable template file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 /**
  * Usage: gulp [--nowatch] [--clipboard]
  */
-var defaultTasks = ['scripts', 'html', 'styles', 'watch'];
+// note that HTML is not in the list because both scripts and styles call HTML when they're done
+var defaultTasks = ['scripts', 'styles', 'watch'];
 
 var argv = require('yargs').argv;
 var gulp = require('gulp');
@@ -12,40 +13,20 @@ var preprocess = require('gulp-preprocess');
 var compass = require('gulp-compass');
 var watch = require('gulp-watch');
 var clipboard = require('gulp-clipboard');
-var minifyCss = require('gulp-minify-css');
 var source = require('vinyl-source-stream');
 
 gulp.task('scripts', function () {
-    return browserify('./theme/js/main.js')
-        .bundle()
-        .on('error', watchScripts) // restart watch task on error
-        //Pass desired output filename to vinyl-source-stream
-        .pipe(source('./build/theme.js'))
-        // Start piping stream to tasks!
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('html', function () {
-    gulp.src('theme/templates/main.tumblr')
-        .pipe(preprocess())
-        .on('error', watchHtml) // restart watch task on error
-        .pipe(gulpif(argv.clipboard, clipboard()))
-	    .pipe(rename('theme.tumblr'))
-        .pipe(gulp.dest('build/'));
+    return compileScripts();
 });
 
 gulp.task('styles', function () {
-    gulp.src('theme/sass/*.scss')
-        .pipe(compass({
-            config_file: 'config.rb',
-            css: 'build',
-            sass: 'theme/sass',
-            import_path: 'theme/libs/bower_components/foundation/scss'
-        }))
-        .on('error', watchStyles)
-		.pipe(minifyCss())
-        .pipe(gulp.dest('build/'));
+    return compileStyles();
 });
+
+gulp.task('html', function () {
+    return compileHtml();
+});
+
 
 gulp.task('watch', watchTask);
 
@@ -55,18 +36,91 @@ if (argv.nowatch) {
 
 gulp.task('default', defaultTasks);
 
+/**
+ * Compile HTML
+ *
+ * 'html' task code
+ *
+ * @returns {*}
+ */
+function compileHtml()
+{
+    return gulp.src('theme/templates/main.tumblr')
+        .pipe(preprocess())
+        .on('error', watchHtml) // restart watch task on error
+        .pipe(gulpif(argv.clipboard, clipboard()))
+        .pipe(rename('theme.tumblr'))
+        .pipe(gulp.dest('dist/'));
+}
 
-/** Helper Functions **/
+/**
+ * Compile Styles
+ *
+ * 'styles' task code
+ *
+ * @returns {*}
+ */
+function compileStyles()
+{
+    var styles = gulp.src('theme/sass/*.scss')
+        .pipe(compass({
+            config_file: 'config.rb',
+            css: 'build',
+            sass: 'theme/sass',
+            import_path: 'theme/libs/bower_components/foundation/scss'
+        }))
+        .on('error', watchStyles)
+        .pipe(gulp.dest('build/'));
+
+    return styles;
+}
+
+/**
+ * Compile Scripts
+ *
+ * 'scripts' task code
+ *
+ * @returns {*}
+ */
+function compileScripts()
+{
+    var scripts = browserify('./theme/js/main.js')
+        .bundle()
+        .on('error', watchScripts) // restart watch task on error
+        //Pass desired output filename to vinyl-source-stream
+        .pipe(source('./build/theme.js'))
+        .pipe(gulp.dest("./"));
+
+    return scripts;
+}
+
+
+/** Setup watchers **/
+
+/**
+ * Watch Task
+ *
+ * Starts all watchers
+ *
+ * @returns void
+ */
 function watchTask(errorMsg) {
 	if (argv.nowatch) {
 		return;
 	}
 
     watchStyles(errorMsg);
-    watchHtml(errorMsg);
     watchScripts(errorMsg);
+    watchHtml(errorMsg);
 }
 
+/**
+ * Watch Styles
+ *
+ * Setups up a watcher on the sass files
+ *
+ * @returns void
+ */
 function watchStyles(errorMsg) {
 	if (argv.nowatch) {
 		return;
@@ -76,15 +130,29 @@ function watchStyles(errorMsg) {
     logError(errorMsg);
 }
 
+/**
+ * Watch HTML
+ *
+ * Sets up a watcher on the HTML template partials
+ *
+ * @returns void
+ */
 function watchHtml(errorMsg) {
 	if (argv.nowatch) {
 		return;
 	}
 
-    gulp.watch('theme/templates/**/*.tumblr', ['html']);
+    gulp.watch(['build/theme.js', 'build/theme.css', 'theme/templates/**/*.tumblr'], ['html']);
     logError(errorMsg);
 }
 
+/**
+ * Watch Scripts
+ *
+ * Sets up a watcher on the javascript libraries etc.
+ *
+ * @returns void
+ */
 function watchScripts(errorMsg) {
 	if (argv.nowatch) {
 		return;
@@ -94,6 +162,13 @@ function watchScripts(errorMsg) {
     logError(errorMsg);
 }
 
+/**
+ * Log Error
+ *
+ * Simple console.log wrapper to avoid logging undefined messages
+ *
+ * @param string errorMsg the message to log
+ */
 function logError(errorMsg) {
     if (errorMsg) {
         console.log(errorMsg);

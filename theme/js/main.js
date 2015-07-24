5d561f065e70c3363fffa71f73858adb02859497
Chad Furman <chad@chadfurman.com>
2015-07-24 19:11:18 -0400

    jquery is coming out of this project, removing it from the js file

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,21 +1,21 @@
-require('tumblrPhotoset');
-require('infiniteScroll');
-
-
-$(document).ready(function () {
-	$(document).foundation();
-	$('.tumblr-photoset').each(function () {
-		$(this).tumblrPhotoset();
-	});
-
-	$('.posts').infinitescroll({
-		navSelector: ".pagination",
-		nextSelector: ".next-page",
-		itemSelector: ".post",
-		bufferPx: 400
-	}, function (newElementsArray) {
-		$('.tumblr-photoset', newElementsArray).each(function () {
-			$(this).tumblrPhotoset();
-		});
-	});
-});
+//require('tumblrPhotoset');
+//require('infiniteScroll');
+//
+//
+//$(document).ready(function () {
+//	$(document).foundation();
+//	$('.tumblr-photoset').each(function () {
+//		$(this).tumblrPhotoset();
+//	});
+//
+//	$('.posts').infinitescroll({
+//		navSelector: ".pagination",
+//		nextSelector: ".next-page",
+//		itemSelector: ".post",
+//		bufferPx: 400
+//	}, function (newElementsArray) {
+//		$('.tumblr-photoset', newElementsArray).each(function () {
+//			$(this).tumblrPhotoset();
+//		});
+//	});
+//});

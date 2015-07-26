7ff59235f67f6049453156850ce891143b6c9da4
Chad Furman <chad@chadfurman.com>
2015-07-26 03:21:39 -0400

    removed jquery from rollover

--- theme/libs/tumblr-photoset/tumblr-photoset.album-photoset.rollover.js
+++ theme/libs/tumblr-photoset/tumblr-photoset.album-photoset.rollover.js
@@ -1,37 +1,36 @@
 /**
- * Tumblr Album Photoset Lightbox (jQuery)
+ *
+ * Tumblr Album Photoset Lightbox
  *
  * Inspired by: https://github.com/PixelUnion/Extended-Tumblr-Photoset
  */
 
-var $ = require('jquery');
 var albumPhotoset = require('tumblrPhotoset.albumPhotoset');
 
 albumPhotoset.registerEventHandler('row-ready', function (albumPhotosetInstance, row) {
-	$row = $(row[0]);
-	baseImages = $row.find('img:even');
-	rolloverImages = $row.find('img:odd');
+	row = row[0];
+	var baseImages = row.getElementsByTagName('IMG'),
+		rolloverImages = [];
 
-	baseImages.each(function (index) {
-		if (rolloverImage = rolloverImages[index]) {
-			var baseImage = baseImages[index];
-			var $baseImage = $(baseImage);
-			var baseImageSrc = baseImage.src;
-			var rolloverImage = rolloverImages[index];
-			var $rolloverImage = $(rolloverImage);
-			var rolloverImageSrc = rolloverImages[index].src;
+	console.log('baseImages:', baseImages);
+	for (var imageCounter = 0; imageCounter < baseImages.length; imageCounter++ ) {
+		var image = baseImages[imageCounter];
+		console.log('image:', image, image.getAttribute('data-caption'));
+		if (image.getAttribute('data-caption').match(/#rollover/)) {
+			var baseImage = baseImages[imageCounter - 1],
+				rolloverImageSrc = image.getAttribute('src'),
+				baseImageSrc = image.getAttribute('src');
+			console.log('rolloverSrc', rolloverImageSrc);
+			console.log('baseSrc', baseImageSrc);
 
-			$baseImage
-				.on('mouseover', function () {
-					this.src = rolloverImageSrc;
-				})
-				.on('mouseout', function () {
-					this.src = baseImageSrc;
-				});
+			baseImage.addEventListener('mouseover', function () { this.src = rolloverImageSrc; });
+			baseImage.addEventListener('mouseout', function () { this.src = baseImageSrc; });
 
-			$rolloverImage.parent().remove();
-			$rolloverImage.remove();
+			baseImages.splice(imageCounter, 1);
+			imageCounter--;
 		}
-	});
-	$row.attr('class', 'medium-block-grid-' + baseImages.length);
+	}
+	console.log('rollovers:', rolloverImages); // TODO: mod even
+	console.log('bases:', baseImages); // TODO: mod even
 });
+console.log('rollover registered');

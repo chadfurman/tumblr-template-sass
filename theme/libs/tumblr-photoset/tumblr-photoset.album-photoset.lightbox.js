4e7930e4a20130d60b6f48f5f3434b077dcee2ff
Chad Furman <chad@chadfurman.com>
2015-07-26 03:21:22 -0400

    removed jquery from lightbox

--- theme/libs/tumblr-photoset/tumblr-photoset.album-photoset.lightbox.js
+++ theme/libs/tumblr-photoset/tumblr-photoset.album-photoset.lightbox.js
@@ -4,24 +4,22 @@
  * Inspired by: https://github.com/PixelUnion/Extended-Tumblr-Photoset
  */
 
-var $ = require('jquery');
 var albumPhotoset = require('tumblrPhotoset.albumPhotoset');
 albumPhotoset.lightboxImageArray = {};
 
 albumPhotoset.registerEventHandler('post-init', function (albumPhotosetInstance) {
-	var photosetId = albumPhotosetInstance.$photoset.attr('id');
-	albumPhotosetInstance.lightboxImageArray[photosetId] = [];
+	var photosetId = albumPhotosetInstance.photoset.attr('id');
+	var photosetImages = albumPhotosetInstance.photosetImages;
 
-	var imageCounter = 0;
-	albumPhotosetInstance.$photosetImages.each(function () {
-		$this = $(this);
-		$this.data('position', ++imageCounter);
+	for (var imageCounter = 1; imageCounter <= photosetImages.length; imageCounter++) {
+		var image = photosetImages[imageCounter];
+		image.setAttribute('data-position', imageCounter);
 
 		// extract properties for tumblr lightbox
-		var imageWidth = $this.data('width');
-		var imageHeight = $this.data('height');
-		var imageLowRes = $this.attr('src');
-		var imageHighRes = $this.data('highres');
+		var imageWidth = image.getAttribute('data-width');
+		var imageHeight = image.getAttribute('data-height');
+		var imageLowRes = image.getAttribute('data-src');
+		var imageHighRes = image.getAttribute('data-highres');
 
 		// add image properties to lightbox array
 		albumPhotosetInstance.lightboxImageArray[photosetId].push({
@@ -31,8 +29,8 @@ albumPhotoset.registerEventHandler('post-init', function (albumPhotosetInstance)
 			high_res: imageHighRes
 		});
 
-		$this.on('click', function () {
-			Tumblr.Lightbox.init(albumPhotosetInstance.lightboxImageArray[photosetId], $(this).data('position'));
+		image.addEventListener('click', function () {
+			Tumblr.Lightbox.init(albumPhotosetInstance.lightboxImageArray[photosetId], image.getAttribute('data-position'));
 		});
-	})
+	}
 });

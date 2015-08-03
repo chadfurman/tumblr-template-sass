e76f9e90a810f99305423db69769127c53e27f9a
Chad Furman <chad@chadfurman.com>
2015-08-02 21:53:46 -0400

    added helper methods to photoset

--- theme/libs/tumblr-photoset/tumblr-photoset.album-photoset.js
+++ theme/libs/tumblr-photoset/tumblr-photoset.album-photoset.js
@@ -15,6 +15,7 @@ var albumPhotoset = {
 	 * every property is a list of methods, property names being the events that trigger the methods
 	 */
 	callbacks: {},
+	rowStart: [],
 
 	/**
 	 * Init
@@ -118,6 +119,63 @@ var albumPhotoset = {
 			imagePositionPointer += numImagesInRow;
 		} // end row loop
 		this.triggerEvent('post-render', this.photoset);
-	} // end render()
+	}, // end render()
+
+	/**
+	 * Image Iterator
+	 * Executes the callback for each image, passing in the index's image in the array and the current row number
+	 * @param callback Return true to remove the current image from the dom, from the image array, and from the row
+	 */
+	imageIterator: function (callback) {
+		var rowCounter = 0;
+		var baseImages = this.photosetImages;
+
+		// iterate over every image in the baseImages array and check for rollover triggers
+		for (var imageCounter = 1; imageCounter < baseImages.length; imageCounter++ ) {
+			if (imageCounter > this.layout[rowCounter]) {
+				// keep track of what row we're on in the linear image list
+				rowCounter++;
+			}
+
+			if (callback(imageCounter, rowCounter)) { //
+				this.removeImageAtIndexFromRow(imageCounter, rowCounter);
+				imageCounter--; // we now have one less image total, but the same number remaining
+			}
+		}
+	},
+
+	/**
+	 * Remove Image At Index From Row
+	 * Removes the specified image from the dom, from the photosetImages array, and from the layout array
+	 * @param imageIndex position in the this.photosetImages array
+	 * @param row position in the this.layout array
+	 */
+	removeImageAtIndexFromRow: function (imageIndex, row) {
+		var image = this.photosetImages[imageIndex];
+		image.parentElement.removeChild(image); // remove rollover image from dom
+		this.photosetImages.splice(imageIndex, 1); // remove rollover image from the image array
+		this.layout[row]--; // shorten expected row length by 1
+	},
+
+	/**
+	 * Get Start Of Row
+	 * Given a row number, calculate the position in the photosetImages array of the first image in the row
+	 * Caches the calculation for faster lookup
+	 * @param rowNumber
+	 * @returns {*}
+	 */
+	getStartOfRow: function (rowNumber) {
+		if (typeof this.rowStart[rowNumber] !== 'undefined') {
+			return this.rowStart[rowNumber]; // cache
+		}
+
+		var rowStart = 0;
+		for (var i = 0; i < rowNumber; i++) {
+			rowStart = rowStart + this.layout[i];
+		}
+
+		this.rowStart[rowNumber] = rowStart;
+		return rowStart;
+	}
 };
 module.exports = albumPhotoset;
